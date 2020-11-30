fcf87774f439c41693ad697bdf6521486b9fc70a
Harley Hanna <harleyhannawrit@gmail.com>
2020-11-30 19:19:42 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,7 +34,7 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
-    
+    @pet.update(params["pet"])
     redirect to "pets/#{@pet.id}"
   end
 end
