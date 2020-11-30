ee5ff5186a7d41ddda1cdc1eba85fe8145069da7
Harley Hanna <harleyhannawrit@gmail.com>
2020-11-30 19:16:47 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,8 @@ class PetsController < ApplicationController
     @pet = Pet.create(params[:pet])
     
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
